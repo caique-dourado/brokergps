dffd74801a6045d0e3514468777f9c6412b0d4c5
Caique Dourado <eng.caiquedourado@gmail.com>
2021-12-16 12:12:44 -0300

    Inserindo a classe MyHttpOverides para corrigir bug de conexao

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'ui/ui_gpsbroker.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -19,5 +22,20 @@ class MyApp extends StatelessWidget {
       ),
       home: const UiGps(title: 'Broker São Luis'),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  /* HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+(X509Certificate cert, String host, int port) = true;
+  } */
+
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
