49004c7ca088e50c49448af2c42e65557aa97bb5
Caique Dourado <eng.caiquedourado@gmail.com>
2021-12-16 12:11:32 -0300

    implementando o botão de insert

--- lib/ui/ui_gpsbroker.dart
+++ lib/ui/ui_gpsbroker.dart
@@ -1,14 +1,23 @@
-import 'package:brokergps/blocs/camera.dart';
-import 'package:brokergps/blocs/send_gpsbroker.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 
+import 'package:brokergps/blocs/camera.dart';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:location/location.dart';
 
 TextStyle style = const TextStyle(fontFamily: "Roboto", fontSize: 14.0);
+TextStyle styleFontCoord =
+    const TextStyle(fontFamily: "Roboto", fontSize: 20.0);
 const espacamento = Padding(padding: EdgeInsets.all(6));
 TextEditingController soldCliente = TextEditingController();
 TextEditingController lat = TextEditingController();
 late Location _localizacao = Location();
+bool _loading = false;
+String _status = 'Não enviado';
+double progresValue = 0;
+late String coordenadas;
 
 class UiGps extends StatefulWidget {
   const UiGps({Key? key, required this.title}) : super(key: key);
@@ -44,138 +53,253 @@ class _UiGpsState extends State<UiGps> {
     }
   }
 
+  Future _insert() async {
+    try {
+      var response = await http.post(
+          Uri.parse('https://brokersaoluis.com.br/apis-appgps/insert.php'),
+          body: {
+            "sold": soldCliente.text.toString(),
+            "coord": coordenadas.toString(),
+            "foto_faixada": imgAutenticacao.toString()
+          });
+      setState(() {
+        const oneSec = Duration(milliseconds: 50);
+        Timer.periodic(oneSec, (Timer t) {
+          setState(() {
+            progresValue += 0.1;
+            if (progresValue.toStringAsFixed(1) == '1.0') {
+              if (response.statusCode == 200) {
+                _loading = false;
+                _status = "Coordenadas Enviadas";
+                t.cancel();
+                progresValue = 0.0;
+                showDialog<String>(
+                    context: context,
+                    builder: (BuildContext context) => AlertDialog(
+                          title: const Text("Aviso"),
+                          content: const Text(
+                            "Coordenadas enviadas",
+                            textAlign: TextAlign.center,
+                          ),
+                          actions: [
+                            ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.white),
+                                    shadowColor: MaterialStateProperty.all(
+                                        Colors.transparent),
+                                    textStyle: MaterialStateProperty.all(
+                                        const TextStyle(
+                                            fontSize: 14,
+                                            fontFamily: 'Roboto',
+                                            color: Colors.black))),
+                                onPressed: () =>
+                                    Navigator.pop(context, 'Cancel'),
+                                child: const Text('Ok',
+                                    style: TextStyle(color: Color(0xff2A4968))))
+                          ],
+                        ));
+              }
+            }
+          });
+        });
+      });
+      if (response.statusCode != 200) {
+        throw HttpException('${response.statusCode}');
+      }
+    } on SocketException {
+      setState(() {
+        _status = 'Sem conexão com internet';
+      });
+    }
+  }
+
+  Future _verificacao() async {
+    if (storedImage == null) {
+      showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                title: const Text("Aviso"),
+                content: const Text(
+                  "Prezado vendedor, realize a captura da faixada do cliente",
+                  textAlign: TextAlign.center,
+                ),
+                actions: [
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                          shadowColor:
+                              MaterialStateProperty.all(Colors.transparent),
+                          textStyle: MaterialStateProperty.all(const TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'Roboto',
+                              color: Colors.black))),
+                      onPressed: () => Navigator.pop(context, 'Cancel'),
+                      child: const Text('Ok',
+                          style: TextStyle(color: Color(0xff2A4968))))
+                ],
+              ));
+    } else {
+      setState(() {
+        _loading = true;
+        _insert();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(6.0, 14.0, 6.0, 1.0),
-        color: Colors.transparent,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Flexible(
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          style: style,
-                          controller: soldCliente,
-                          onChanged: (text) => {},
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(6),
-                            labelText: "Sold do cliente",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15.0)),
+            resizeToAvoidBottomInset: false,
+            body: Container(
+                padding: const EdgeInsets.fromLTRB(6.0, 14.0, 6.0, 1.0),
+                color: Colors.transparent,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            style: style,
+                            controller: soldCliente,
+                            onChanged: (text) => {},
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(6),
+                              labelText: "Sold do cliente",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                            ),
                           ),
                         ),
-                      ),
-                      espacamento,
-                      Flexible(
-                          child: IconButton(
-                        icon: const Icon(Icons.gps_fixed),
-                        onPressed: () {
-                          pegarLocalizacao();
-                        },
-                        iconSize: 30.0,
-                        color: const Color(0xff2A4968),
-                      )),
-                    ],
-                  ),
-                  espacamento,
-                  Row(
-                    children: <Widget>[
-                      Flexible(
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          style: style,
-                          enabled: false,
-                          controller: TextEditingController()
-                            ..text = _StringPosicaoLat,
-                          onChanged: (text) => {},
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(6),
-                            labelText: "Latitude",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15.0)),
-                          ),
-                        ),
-                      ),
-                      espacamento,
-                      Flexible(
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          style: style,
-                          enabled: false,
-                          controller: TextEditingController()
-                            ..text = _StringPosicaoLong,
-                          onChanged: (text) => {},
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(6),
-                            labelText: "Longitude",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15.0)),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  espacamento,
-                  Row(children: const [
-                    Text('Coordenadas*',
-                        style: TextStyle(
-                          color: Colors.red,
+                        espacamento,
+                        Flexible(
+                            child: IconButton(
+                          icon: const Icon(Icons.gps_fixed),
+                          onPressed: () {
+                            FocusScope.of(context).requestFocus(FocusNode());
+                            pegarLocalizacao();
+                          },
+                          iconSize: 30.0,
+                          color: const Color(0xff2A4968),
                         )),
-                  ]),
-                  _StringPosicaoLat != ''
-                      ? Text(
-                          "|${soldCliente.text}||||$_StringPosicaoLat|$_StringPosicaoLong|S")
-                      : const Text(''),
-                  espacamento,
-                  Row(
-                    children: <Widget>[
-                      Builder(builder: (context) {
-                        return const Text('Foto da faixada do cliente*',
-                            style: TextStyle(
-                              color: Colors.red,
-                            ));
-                      }),
-                    ],
-                  ),
-                  espacamento,
-                  SizedBox(
-                    child: Row(
-                      children: const [AbrirCamera()],
+                      ],
                     ),
-                  ),
-                  espacamento,
-                  espacamento,
-                  Row(
-                    children: const [
-                      Text(
-                          'INSTRUÇÕES DE USO:\n1- Insira o sold do cliente.\n2- Pressione no botão do gps (Ao lado de Sold).\n3- Tire a foto da faixada do cliente.\n4- Pressionar no botão enviar')
-                    ],
-                  ),
-                  espacamento,
-                  espacamento,
-                  espacamento,
-                  // ignore: unrelated_type_equality_checks
+                    espacamento,
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            style: style,
+                            enabled: false,
+                            controller: TextEditingController()
+                              ..text = _StringPosicaoLat,
+                            onChanged: (text) => {},
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(6),
+                              labelText: "Latitude",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                            ),
+                          ),
+                        ),
+                        espacamento,
+                        Flexible(
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            style: style,
+                            enabled: false,
+                            controller: TextEditingController()
+                              ..text = _StringPosicaoLong,
+                            onChanged: (text) => {},
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(6),
+                              labelText: "Longitude",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    espacamento,
+                    Row(children: const [
+                      Text('Coordenadas*',
+                          style: TextStyle(
+                            color: Colors.red,
+                          )),
+                    ]),
+                    espacamento,
+                    _StringPosicaoLat != ''
+                        ? Text(
+                            "|${soldCliente.text}||||$_StringPosicaoLat|$_StringPosicaoLong|S",
+                            style: styleFontCoord,
+                          )
+                        : const Text(''),
+                    espacamento,
+                    Row(
+                      children: <Widget>[
+                        Builder(builder: (context) {
+                          return const Text('Foto da faixada do cliente*',
+                              style: TextStyle(
+                                color: Colors.red,
+                              ));
+                        }),
+                      ],
+                    ),
+                    espacamento,
+                    SizedBox(
+                      child: Row(
+                        children: const [AbrirCamera()],
+                      ),
+                    ),
 
-                  SizedBox(
-                    child: Row(
-                      children: const [EnviarCoord()],
+                    espacamento,
+                    // ignore: unrelated_type_equality_checks
+                    Card(
+                        elevation: 4,
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                                title: const Text('Coordenadas do Cliente'),
+                                subtitle: Text('Status: $_status')),
+                            _loading ? _linearProgressIndicator() : Container(),
+                            Text('${progresValue.toStringAsPrecision(2)}%')
+                          ],
+                        )),
+                    const Spacer(
+                      flex: 1,
                     ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    ));
+                    Row(
+                      children: [
+                        espacamento,
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          height: 40,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                FocusScope.of(context)
+                                    .requestFocus(FocusNode());
+                                _verificacao();
+                              },
+                              child: const Text('Enviar'),
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.green))),
+                        ),
+                        espacamento,
+                      ],
+                    ),
+
+                    espacamento
+                  ],
+                ))));
   }
 
   pegarLocalizacao() async {
@@ -185,10 +309,19 @@ class _UiGpsState extends State<UiGps> {
       setState(() {
         _StringPosicaoLat = "${posicao.latitude}";
         _StringPosicaoLong = "${posicao.longitude}";
+        coordenadas =
+            "|${soldCliente.text}||||$_StringPosicaoLat|$_StringPosicaoLong|S";
       });
     }).catchError((e) {
       _StringPosicaoLat = e.toString();
       _StringPosicaoLong = e.toString();
     });
+  }
+
+  Widget _linearProgressIndicator() {
+    return LinearProgressIndicator(
+      valueColor: const AlwaysStoppedAnimation<Color>(Color(0xff2A4968)),
+      value: progresValue,
+    );
   }
 }
